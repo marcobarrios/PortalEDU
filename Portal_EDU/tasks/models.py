ba1310c3266c3fb73d745d911926a2a59cc67fef
Marco Barrios <mabarriosmarco@gmail.com>
2015-06-29 18:36:35 -0600

    Update DataBase diagram

--- Portal_EDU/tasks/models.py
+++ Portal_EDU/tasks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Task(models.Model):
-    delivered_date_task = models.DateTimeField()
+    delivered_date_task = models.DateTimeField(auto_now=True)
     checked_task = models.BooleanField(default=0)
     file_task = models.FileField(upload_to='task_files/%Y/%m/%d/', blank=True)
     teacher_commentary_task = models.TextField(blank=True) 

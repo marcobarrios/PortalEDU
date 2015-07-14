2856a30a7ff3eb912ab569c5a426c191091dd1b5
Marco Barrios <mabarriosmarco@gmail.com>
2015-07-14 12:36:44 -0600

    Addded assists app

---
+++ Portal_EDU/assists/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Assistance(models.Model):
+	title_assistance = models.CharField(max_lenght=45, blank=True, null=True)
+	date_assistance = models.DateField()
+
+	course = models.ForeignKey('courses.Course')
+	students = models.ManyToManyField('students.Student')
+
+	def __unicode__(self):
+		return self.title_assistance + " " + str(self.date_assistance) + " " + str(self.course)
+

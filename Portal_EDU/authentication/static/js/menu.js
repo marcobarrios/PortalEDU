98e9e87d54e2a31af8d51c28c70bd3481341a2b4
gerson <gerson1vime@gmail.com>
2015-08-06 12:14:54 -0600

    Add css, the index , authentication

---
+++ Portal_EDU/authentication/static/js/menu.js
@@ -0,0 +1,29 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.bt_login').click(function(){
+			contador = 1; 
+			$('nav').animate({
+				left: '-100%'
+			});
+		
+	});
+	$('.bt_menu').click(function(){
+		//$('nav').toggle();
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		}else{
+			contador = 1; 
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	
+};
